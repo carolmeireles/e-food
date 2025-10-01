c2292952071deae5463191d0d88b4783c7cf1939
carolmeireles <carolinemeireles.si@gmail.com>
2025-10-01 11:31:59 -0300

    store e reducer

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { Restaurante } from "../pages/Home"
+
+const api = createApi({
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://ebac-fake-api.vercel.app/api/efood'
+    }),
+    endpoints: (builder) => ({
+        getRestaurante: builder.query<Restaurante, string>({
+            query: (id) => `restaurantes/${id}`
+        }),
+        getListaRestaurantes: builder.query<Restaurante[], void>({
+            query: () => 'restaurantes'
+        })
+    })
+})
+
+export const { useGetRestauranteQuery, useGetListaRestaurantesQuery } = api
+export default api
