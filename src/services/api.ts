54a590fbe867b822cb0a51350f6580593aa89d1d
carolmeireles <carolinemeireles.si@gmail.com>
2025-11-03 12:47:00 -0300

    cart, api e checkout page (inicial)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,40 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { Restaurante } from "../pages/Home"
 
+type Product = {
+    id: number
+    price: number
+}
+
+type PurchasePayload = {
+    products: Product[]
+    delivery: {
+        receiver: string,
+        address: {
+            description: string
+            city: string
+            zipCode: string
+            number: number
+            complement: string
+        },
+        payment: {
+            card: {
+                name: string
+                number: string
+                code: number
+                expires: {
+                    month: number
+                    year: number
+                }
+            }
+        }
+    }
+}
+
+type PurchaseResponse = {
+    orderId: string
+}
+
 const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://api-ebac.vercel.app/api/efood'
@@ -11,9 +45,16 @@ const api = createApi({
         }),
         getListaRestaurantes: builder.query<Restaurante[], void>({
             query: () => 'restaurantes'
+        }),
+        purchase: builder.mutation<PurchaseResponse, PurchasePayload>({
+            query: (body) => ({
+                url: 'checkout',
+                method: 'POST',
+                body
+            })
         })
     })
 })
 
-export const { useGetRestauranteQuery, useGetListaRestaurantesQuery } = api
+export const { useGetRestauranteQuery, useGetListaRestaurantesQuery, usePurchaseMutation } = api
 export default api
