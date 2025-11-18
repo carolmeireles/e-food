acfae2ed40ebe8389b5c94f7f21edf6da134c09e
carolmeireles <carolinemeireles.si@gmail.com>
2025-11-18 11:55:57 -0300

    remocao do link no componente button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { cores } from "../../styles";
-import { Link } from "react-router-dom";
 
 export const Btn = styled.button`
   width: 100%;
@@ -20,17 +19,4 @@ export const Btn = styled.button`
 
 export const Container = styled.div`
   display: relative;
-`;
-
-export const BtnLink = styled(Link)`
-  background-color: ${cores.rosa};
-  color: ${cores.begeClaro};
-  padding: 6px 4px;
-  font-weight: bold;
-  font-size: 14px;
-  border: none;
-  cursor: pointer;
-  margin: 0 0 8px 8px;
-  display: inline-block;
-  text-decoration: none;
 `;
