9f7c17051359d28d44530c4339b067734ececb72
carolmeireles <carolinemeireles.si@gmail.com>
2025-11-24 11:33:54 -0300

    Revert "criacao do arquivo de tipos"
    This reverts commit ec30440aca80ddc8046cb61e246c6405522132f6.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,26 @@ import CardList from "../../components/CardList";
 import Hero from "../../components/Hero";
 import { useGetListaRestaurantesQuery } from "../../services/api";
 
+export interface Cardapio {
+  foto: string;
+  preco: number;
+  id: number;
+  nome: string;
+  descricao: string;
+  porcao: string;
+}
+
+export type Restaurante = {
+  id: number;
+  titulo: string;
+  destacado: boolean;
+  tipo: string;
+  avaliacao: number;
+  descricao: string;
+  capa: string;
+  cardapio: Cardapio[];
+};
+
 const Home = () => {
   const { data: listaRestaurantes } = useGetListaRestaurantesQuery();
 
